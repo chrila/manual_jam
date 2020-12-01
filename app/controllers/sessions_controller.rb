534b0b6dd373f09c5964fa4e6bf9f4d4c7815f24
Christian <christian.lampl@gmx.at>
2020-11-30 23:08:31 -0300

    Add: user and session management

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+
+    respond_to do |format|
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        format.html { redirect_to root_path, notice: 'Signed in successfully.' }
+      else
+        format.html { redirect_to new_session_path, alert: 'email and password don\'t match' }
+      end
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: 'Logged out successfully.' }
+    end
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    respond_to do |format|
+      if @user.save
+        session[:user_id] = @user.id
+        format.html { redirect_to root_path, notice: 'User created successfully.' }
+      else
+        format.html { redirect_to new_user_path, alert: 'User could not be saved.' }
+      end
+    end
+  end
+
+  def show
+    @user = User.find(session[:user_id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
